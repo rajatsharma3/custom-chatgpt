7e629f6812c0951f89435bd11417a9128dadeb36
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-03-06 17:00:24 +0530

    supabase vector db get and post api created

--- backend/custom-chatgpt/supabase.js
+++ backend/custom-chatgpt/supabase.js
@@ -1,20 +1,27 @@
 import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
-import { OpenAIEmbeddings } from "@langchain/openai";
+import { OpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { createClient } from "@supabase/supabase-js";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { RetrievalQAChain, loadQAStuffChain } from "langchain/chains";
 import { config } from "dotenv";
 config();
 
-const privateKey = process.env.SUPABASE_PRIVATE_KEY;
-if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
+const supabaseKey = process.env.SUPABASE_PRIVATE_KEY;
+if (!supabaseKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
 
-const url = process.env.SUPABASE_URL;
-if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+const supabaseUrl = process.env.SUPABASE_URL;
+if (!supabaseUrl) throw new Error(`Expected env var SUPABASE_URL`);
+
+const client = createClient(supabaseUrl, supabaseKey);
+const vectorStore = new SupabaseVectorStore(new OpenAIEmbeddings(), {
+  client,
+  tableName: "documents",
+});
 
 export const supabasePost = async (req, res) => {
   try {
-    const loader = new PDFLoader("custom-chatgpt/drapcodeintro.pdf");
+    const loader = new PDFLoader("custom-chatgpt/documents/drapcodeintro.pdf");
 
     const docs = await loader.load();
 
@@ -25,16 +32,7 @@ export const supabasePost = async (req, res) => {
 
     const splittedDocs = await splitter.splitDocuments(docs);
 
-    const client = createClient(url, privateKey);
-
-    const embeddings = new OpenAIEmbeddings();
-
-    const store = new SupabaseVectorStore(embeddings, {
-      client,
-      tableName: "documents",
-    });
-
-    await store.addDocuments(splittedDocs);
+    await vectorStore.addDocuments(splittedDocs);
 
     res.send({ msg: "document added successfully" });
   } catch (err) {
@@ -45,12 +43,6 @@ export const supabasePost = async (req, res) => {
 
 export const supabaseGet = async (req, res) => {
   try {
-    const client = createClient(url, privateKey);
-    const vectorStore = new SupabaseVectorStore(new OpenAIEmbeddings(), {
-      client,
-      tableName: "documents",
-    });
-
     const model = new OpenAI({ temperature: 0.6 });
 
     const chain = new RetrievalQAChain({
@@ -66,6 +58,7 @@ export const supabaseGet = async (req, res) => {
     console.log(result.text);
     res.send(result.text);
   } catch (err) {
+    console.log(err);
     res.send({ error: err });
   }
 };
